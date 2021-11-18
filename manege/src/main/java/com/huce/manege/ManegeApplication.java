872502e152df44b50dac48729c7afe04a5eaa674
batrung <batrung255@gmail.com>
2021-11-19 00:25:40 +0700

    first commit

---
+++ manege/src/main/java/com/huce/manege/ManegeApplication.java
@@ -0,0 +1,13 @@
+package com.huce.manege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManegeApplication.class, args);
+	}
+
+}
---
+++ manege/src/test/java/com/huce/manege/ManegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huce.manege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
