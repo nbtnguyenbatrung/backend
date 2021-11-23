a3c95c66dd94b9a95ef89cc046706c7a078f85cd
batrung <batrung255@gmail.com>
2021-11-23 23:25:19 +0700

    update

---
+++ miniblogs/src/main/java/com/HUCE/miniblogs/MiniblogsApplication.java
@@ -0,0 +1,13 @@
+package com.HUCE.miniblogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniblogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniblogsApplication.class, args);
+	}
+
+}
---
+++ miniblogs/src/test/java/com/HUCE/miniblogs/MiniblogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HUCE.miniblogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniblogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
